96845bb5561150bd6f43b95b7fc58c70f55b83e1
Ankitsharma991 <sharmaa7861@gmail.com>
2024-01-30 13:33:01 +0530

    updated script

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-  const updateCursor = ({ x, y }) => {
-    document.documentElement.style.setProperty("--x", x);
-    document.documentElement.style.setProperty("--y", y);
-  };
-  
-  document.body.addEventListener("pointermove", updateCursor);
-
-}
